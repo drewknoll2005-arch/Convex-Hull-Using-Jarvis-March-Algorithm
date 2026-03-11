4a7d82c057588756c321454130b9ee3c5f4bbb1f
Andrew Knoll <Aknoll1@mtech.edu>
2026-03-11 12:05:37 -0600

    added orientation function, and disclaimer

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
+"""
+Andrew Curtis Knoll
+CSCI 332 Spring 2025
+Programming Assignment #9
+I acknowledge that I have worked on this assignment independently, except where explicitly noted
+and referenced. Any collaboration or use of external resources has been properly cited. I am
+fully aware of the consequences of academic dishonesty and agree to abide by the university's
+academic integrity policy. I understand the importance the consequences of plagiarism.
+"""
+
 from typing import List, Tuple
+
+def orientation(p1:tuple[int, int],  p2: tuple[int, int], p3: tuple[int, int]):
+    val = ((p2[1]-p1[1]) * (p3[0]-p2[0]) - (p2[0]-p1[0]) *(p3[1]-p2[1]))
+
+    if val == 0:
+        return 0 #Colinear
+    
+    if val > 0:
+        return 1 #Clockwise
+    
+    if val < 0: 
+        return 2 #Counter Clockwise
 
 Point = Tuple[float, float]
 
--- tests.py
+++ tests.py
@@ -1,3 +1,13 @@
+"""
+Andrew Curtis Knoll
+CSCI 332 Spring 2025
+Programming Assignment #9
+I acknowledge that I have worked on this assignment independently, except where explicitly noted
+and referenced. Any collaboration or use of external resources has been properly cited. I am
+fully aware of the consequences of academic dishonesty and agree to abide by the university's
+academic integrity policy. I understand the importance the consequences of plagiarism.
+"""
+
 import unittest
 from main import convex_hull_jarvis
 
